da7b55103f988ec4bbef98dc1255fcfd926b9cd7
Mathyxd <mathias3730@gmail.com>
2026-03-17 11:41:39 +0100

    comparator

--- src/util/OrderComparator.java
+++ src/util/OrderComparator.java
@@ -1,4 +1,20 @@
 package util;
+import Model.Order;
+import java.util.Comparator;
 
-public class OrderComparator {
+public class OrderComparator implements Comparator<Order> {
+    @Override
+    public int compare(Order o1, Order o2){
+
+        int result = o1.getOrderNumber().getPizza()
+                .compareTo(o2.getOrderNumber().getPizza());
+
+        if(result != 0) {
+            return result;
+        }
+
+        return o1.getOrderNumber().getSize()
+                .compareTo(o2.getOrderNumber().getSize());
+
+    }
 }
