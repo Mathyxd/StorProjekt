b357ad19952120ec007be4a4e3f78b9b0fa7219e
idju1000 <idju1000@stud.ek.dk>
2026-03-17 11:23:00 +0100

    gg

--- src/Service/OrderManager.java
+++ src/Service/OrderManager.java
@@ -1,4 +1,31 @@
 package Service;
-
+import java.util.ArrayList;
+import java.util.List;
 public class OrderManager {
-}
+
+        private List<Ordre> ordrer = new ArrayList<>();
+
+        // Kontroller ordre (fx validér data)
+        public boolean kontrollerOrdre(Order ordre) {
+            if (ordre == null) {
+                return false;
+            }
+
+            return true;
+        }
+
+
+        // Beregn samlet pris
+
+        }
+
+        // Beregn rabat
+
+
+        // Gem ordre
+
+
+        // Ekstra (meget nyttigt)
+
+
+
