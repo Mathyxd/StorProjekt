4022fa66ce364495e9ebf9309036a7210a09a8ef
Mathyxd <mathias3730@gmail.com>
2026-03-18 09:55:32 +0100

    ordersorter

--- src/util/OrderSorter.java
+++ src/util/OrderSorter.java
@@ -1,4 +1,14 @@
 package util;
+import Model.Order;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class OrderSorter {
+    public static void sortPizza(ArrayList<Order> order) {
+
+        OrderComparator comparator = new OrderComparator();
+
+        Collections.sort(order, comparator);
+    }
 }
