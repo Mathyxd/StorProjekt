976521febf70a28169ff2604a8ee7e5945e0a79f
Mathyxd <mathias3730@gmail.com>
2026-03-18 09:04:39 +0100

    comparator

--- src/util/OrderComparator.java
+++ src/util/OrderComparator.java
@@ -6,15 +6,13 @@ public class OrderComparator implements Comparator<Order> {
     @Override
     public int compare(Order o1, Order o2){
 
-        int result = o1.getOrderNumber().getPizza()
-                .compareTo(o2.getOrderNumber().getPizza());
+        int result = o1.getPickupTime().compareTo(o2.getPickupTime());
 
         if(result != 0) {
             return result;
         }
 
-        return o1.getOrderNumber().getSize()
-                .compareTo(o2.getOrderNumber().getSize());
+        return o1.getPickupTime().compareTo(o2.getPickupTime());
 
     }
 }
