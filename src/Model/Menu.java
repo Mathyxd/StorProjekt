5d81e060ba358f64eb5006437140b2a456ae7c65
ousmanbah <ouba1000@stud.ek.dk>
2026-03-23 13:27:43 +0100

    update 15

--- src/Model/Menu.java
+++ src/Model/Menu.java
@@ -1,7 +1,6 @@
 package Model;
 
 import File.FileHandler;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,12 +8,10 @@ public class Menu {
     private final List<Pizza> pizzas;
 
     public Menu() {
-        List<Pizza> loadedPizzas = FileHandler.loadMenu();
+        pizzas = FileHandler.loadMenu();
 
-        if (loadedPizzas.isEmpty()) {
-            pizzas = createDefaultMenu();
-        } else {
-            pizzas = loadedPizzas;
+        if (pizzas.isEmpty()) {
+            throw new RuntimeException("Menu kunne ikke indlæses. Tjek menu.csv!");
         }
     }
 
@@ -29,16 +26,5 @@ public class Menu {
             }
         }
         return null;
-    }
-
-    private List<Pizza> createDefaultMenu() {
-        List<Pizza> defaultMenu = new ArrayList<>();
-        defaultMenu.add(new Pizza(1, 11, "Margherita", "Tomat, ost, oregano", 65));
-        defaultMenu.add(new Pizza(2, 12, "Vesuvio", "Tomat, ost, skinke", 72));
-        defaultMenu.add(new Pizza(3, 14, "Capricciosa", "Tomat, ost, skinke, champignon", 78));
-        defaultMenu.add(new Pizza(4, 13, "Pepperoni", "Tomat, ost, pepperoni", 80));
-        defaultMenu.add(new Pizza(5, 13, "Hawaii", "Tomat, ost, skinke, ananas", 79));
-        defaultMenu.add(new Pizza(6, 15, "Napoli", "Tomat, ost, ansjoser, oliven", 81));
-        return defaultMenu;
     }
 }
